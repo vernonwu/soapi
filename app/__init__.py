97c65dfafe67bb59ed1cb2d14a45c09ce5c07990
CuB3y0nd <91041570+CuB3y0nd@users.noreply.github.com>
2025-10-15 18:19:02 +0800

    fix: create database directory if not exist

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 
 from .db import ensure_schema
@@ -9,6 +11,10 @@ def create_app():
         DB_PATH="database/laundry.db",
     )
 
+    db_dir = os.path.dirname(app.config["DB_PATH"])
+    if not os.path.exists(db_dir):
+        os.makedirs(db_dir, exist_ok=True)
+
     ensure_schema(app.config["DB_PATH"])
 
     # register routes
@@ -17,4 +23,3 @@ def create_app():
     app.register_blueprint(bp)
 
     return app
-
